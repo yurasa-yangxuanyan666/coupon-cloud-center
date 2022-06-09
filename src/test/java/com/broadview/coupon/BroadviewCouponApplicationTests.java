ebd0c2ced712e80c462ca1edbccff9e7c5fe93ab
yanghui <yangxuanyan1@gmail.com>
2022-06-09 18:24:55 +0800

    Initial commit

---
+++ src/test/java/com/broadview/coupon/BroadviewCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.broadview.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BroadviewCouponApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
